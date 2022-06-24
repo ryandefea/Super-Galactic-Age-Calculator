d955cc0ce990152aee9615c29215e8e811337a20
ryandefea <ryan.defea@gmail.com>
2022-06-24 14:03:42 -0700

    create failing test for mars calculation.

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -6,8 +6,12 @@ describe('ageCalculator', () => {
     let testPerson = new ageCalculator()
     expect(testPerson.mercuryAge).toBeCloseTo(133.3);
    });
-  test('should return users age om Venus years (32/.62)', () => {
+  test('should return users age in Venus years (32/.62)', () => {
     let testPerson = new ageCalculator();
     expect(testPerson.venusAge).toBeCloseTo(51.6);
-  })
+  });
+  test('should return users age in Mars years (32*1.88)', () => {
+    let testPerson = new ageCalculator();
+    expect(testPerson.marsAge).toBeCloseTo(60.1);
+  });
  });
