c1742d270333ba47968efe1a212e46baa3152ee1
ryandefea <ryan.defea@gmail.com>
2022-06-24 14:20:18 -0700

    create function to pass test, also forgot to fixed1 gets rid of number just after decimal point so had to update test as well.

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -5,6 +5,7 @@ export default class ageCalculator {
       this.usersAge = 32;
       this.mercuryAge = parseFloat((32/.24).toFixed(1));
       this.venusAge = parseFloat((32/.62).toFixed(1));
-      this.marsAge = parseFloat((32*1.88).toFixed(1))
+      this.marsAge = parseFloat((32*1.88).toFixed(1));
+      this.jupiterAge = parseFloat((32*11.86).toFixed(1));
   }
 }
