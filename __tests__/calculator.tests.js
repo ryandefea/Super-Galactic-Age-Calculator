1a1471885cf51820add8224a9eda569cd6655b22
ryandefea <ryan.defea@gmail.com>
2022-06-24 13:20:00 -0700

    fix rounding error.

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -4,6 +4,7 @@ describe('ageCalculator', () => {
    
   test('should return users age in Mercury years (32/.24)', () => {
     let testPerson = new ageCalculator()
-    expect(testPerson.mercuryAge).toBeCloseTo(133.2);
+    expect(testPerson.mercuryAge).toBeCloseTo(133.3);
    });
+
  });
